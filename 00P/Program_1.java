56bd18e875ce1c5918413dead6c76fedabe04dcc
Tarun <srfashions.tarun@gmail.com>
2023-02-19 23:05:57 +0530

    Optimise

--- 00P/Program_1.java
+++ 00P/Program_1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Program_1 {
 	public static void main(String[] args) {
-		int a, b, c; // coefficients
+		int a, b, c; 
 		double root1, root2;
 		System.out.println("Enter the coefficients");
 		Scanner in = new Scanner(System.in);
@@ -17,35 +17,26 @@ public class Program_1 {
 			System.out.println("Not a quadratic equation");
 			return;
 		}
-		// calculate the determinant (b2 - 4ac)
+	
 		double d = (b * b) - (4 * a * c);
 		System.out.println("Determinant= " + d);
-		if (d > 0) // check if determinant is greater than 0
-		{
-
-			// two real and distinct roots
+		if (d > 0)
+{
 			root1 = (-b + Math.sqrt(d)) / (2 * a);
 			root2 = (-b - Math.sqrt(d)) / (2 * a);
 			System.out.println("The roots are real and distinct");
 			System.out.println("root1 = " + root1 + "and root2 = " + root2);
 		}
 
-		else if (d == 0) // check if determinant is equal to 0
+		else if (d == 0)
 		{
-
-			// two real and equal roots
-			// determinant is equal to 0
-			// so -b + 0 == -b
 			root1 = root2 = -b / (2 * a);
 			System.out.println("The roots are real and equal");
 			System.out.println("root1 = root2 = " + root1);
 		}
 
-		else // if determinant is less than zero
+		else
 		{
-
-			// roots are complex number and distinct
-
 			double real = ((double) -b) / (2 * a);
 			double imaginary = Math.sqrt(-d) / (2 * a);
 			System.out.println("The roots are imaginary");
